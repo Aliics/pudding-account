5b08c0543b9e26a46ad985a8025b2aae3edf4c6c
Alexander Johnston <Aliics@hotmail.com>
2019-08-20 17:46:56 +1200

    Setup handler test case

--- src/main/java/fish/eyebrow/puddingaccount/AccountFetchHandler.java
+++ src/main/java/fish/eyebrow/puddingaccount/AccountFetchHandler.java
@@ -6,7 +6,7 @@ import io.vertx.ext.web.RoutingContext;
 public class AccountFetchHandler implements Handler<RoutingContext> {
 
     @Override
-    public void handle(final RoutingContext event) {
-        event.response().end("Hello, World!");
+    public void handle(final RoutingContext context) {
+        context.response().end("Hello, World!");
     }
 }
---
+++ src/test/java/fish/eyebrow/puddingaccount/AccountFetchHandlerTestCase.java
@@ -0,0 +1,39 @@
+package fish.eyebrow.puddingaccount;
+
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AccountFetchHandlerTestCase {
+
+    @Mock
+    private RoutingContext mockContext;
+
+    @Mock
+    private HttpServerResponse mockResponse;
+
+    private AccountFetchHandler accountFetchHandler;
+
+
+    @BeforeEach
+    void setUp() {
+        accountFetchHandler = new AccountFetchHandler();
+
+        Mockito.when(mockContext.response()).thenReturn(mockResponse);
+    }
+
+
+    @Test
+    void fetchingOpenSession() {
+        accountFetchHandler.handle(mockContext);
+
+        Mockito.verify(mockContext.response()).end(ArgumentMatchers.eq("Hello, World!"));
+    }
+}
